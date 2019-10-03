d5d01d3247f96058a57d0ef7d08e2c9d429c097b
ananya2709 <45381289+ananya2709@users.noreply.github.com>
2019-10-03 17:30:31 +0530

    Create findmin and max.py

---
+++ findmin and max.py
@@ -0,0 +1,15 @@
+def minimum(a, n): 
+    
+    while(l<n):
+        minpos = a.index(min(a))
+        i=0,count=0;
+        if(minpos=i):
+    	    count=count
+    	    i++
+        else:
+    	    i++
+    	    count++
+   
+      
+a = [3, 4, 1, 3, 4, 5]  
+minimum(a, len(a)) 
